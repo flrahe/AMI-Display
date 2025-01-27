237a811bc744b0413f1c99c51b1872cc9054eb42
flrahe <florian.rahe@googlemail.com>
2025-01-27 17:44:11 +0100

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,71 @@
+# Display and Logging for Citroen AMI / Opel Rocks-e
+
+
+# ${\color{red}Warning!}$
+${\color{red}Working\space with \space the \space CAN \space on \space your \space car \space can \space be \space very \space dangerous.}$
+${\color{red}This \space is \space just \space for \space research \space purpose.}$
+${\color{red}If \space you \space use \space this, \space you'll \space do \space it \space at \space your \space own \space risk.}$
+
+## Introduction
+The intetion is to display and gather more information from the Citroen AMI. Ist hat two CAN interfaces at the ODB2 plug. The standard one at pins 6 (CAN High)/ 14 (CAN Low) with signal groud at pin 5.
+
+On this bus already some reasearch has been done. This project extends this by two means:
+* Cleaning up the signal definitions
+* Using this information to decode the data on a microcontroller and display it
+
+I'm no professional programmer. So the code will not be very optimized and not very clean. If there is real interest I welcome other to help this project growing.
+
+## Outlook
+This actual state is very basic and just the work of a few hours. Since there is interest in this state, the project is published as it is now. There is no good definitions of pins etc. This all needs to be done. Actually its less than a prove of conceptC and heavily work in progress.
+
+First intention is to get things running. In the second step it might be made "nicer".
+
+A few things which are planned to be included:
+* Publishing different data via MQTT:
+    * directly via WIFI, if available
+    * via bluetooth to a smartphone which acts as bridge to MQTT
+* Logging of CAN raw data (maybe just reduced due to performace/space issues)
+* Using data in the car. i.e. Use the gear information to trigger a reverse camera
+* Deeper alaysis of the "performance" (maybe with erxternal programs which analyze log data)
+    * Battery health
+        * Inner resistance
+        * Monitoring of capacity
+    * Perfoemace reduction monitoring (i.e. with lower temperatures)
+    * Cumulation of consumed and charged power (energy)
+    * Monitoring of 12V battery. Some people reported it got drained.
+
+## Setup
+This project is a PlatformIO project in VSCode. "Just" clone or download this repository and open it in VSCode with PlatformIO plugin installed. This hopefully installs all needed libraries and frameworks.
+
+### secrets.h
+You have to create a secrets.h file in te src directory. An example is put in [secrets_example.h](./src/secrets_example.h). Here you have to put your wifi credentials. The secrets.h is in the gitignore not to accidentially upload it to github.
+
+## Wiring and Hardware
+Actually it is based on an m5Stack core (tested on m5Stack grey), which I had laying around. Since I'm using M5Unified, M5GFX and not using any of the additional hardware of the m5stack grey, it should work on other m5Stacks. Also no sleep mode is used so far. Unfortunately it is seems to be broken on eraly m5. Mine takes 10mA in sleep mode, which is way too high for longer use. It makes even no difference in deep sleep or light sleep.
+
+### Addidiona hardware needed
+#### RTC
+Since there is no RTC in the m5 Core, I added a DS3231 wired to a second i2c bus. The second bus is needed, because there exists already i2c devices on the first bus having the same adress.
+
+ Wiring:
+* PIN17 -> SDA of RTC
+* PIN16 -> SCL of RTC
+
+There exists very small ones of these RTC modules, where you can solder the backup battery to the site, so it fits on an m5stack bus shield.
+
+Newer m5 have an RTC included, but it is actually not used in this version of the software. Actually you may need to manually deactivate the clock stuff or rewitre it (plans for future).
+
+#### Can module
+I'm using [Unit CAN](https://docs.m5stack.com/en/unit/can) for CAN communication. I tested the COMMU module, but I could not get the MCP2515 working. Several other reported problems with that module too. The Unit CAN worked immediatley and so far reliable. And ***"The built-in DC-DC isolated power chip can isolate noise and interference and prevent damage to sensitive circuits."*** So it seems to be a good choice.
+
+The module is connected via UART:
+* RX of module -> PIN36 of m5
+* TX of module -> PIN26 of m5
+
+The wiring of the can side is straightforward:
+* H -> CAN High at ODB2 (pin 6)
+* L -> CAN Low at  ODB2 (pin 14)
+* G -> Signal Ground at ODB" (pin 5).
+
+## CAN data
+In CAN_information [CAN_information](./CAN_information/) you'll find the actual dbc/sym files which help decoding the can frames.
---
+++ src/main.cpp
@@ -0,0 +1,456 @@
+/*
+ * SPDX-FileCopyrightText: 2025 M5Stack Technology CO LTD
+ *
+ * SPDX-License-Identifier: MIT
+ */
+/*
+ * @Hardwares: M5Core + Unit CAN
+ * @Platform Version: Arduino M5Stack Board Manager v2.1.3
+ * @Dependent Library:
+ * M5GFX@^0.2.3: https://github.com/m5stack/M5GFX
+ * M5Unified@^0.2.2: https://github.com/m5stack/M5Unified
+ * ESP32-Arduino-CAN：https://github.com/miwagner/ESP32-Arduino-CAN
+ */
+
+#include <CAN_config.h>
+#include <ESP32CAN.h>
+#include <M5Unified.h>
+#include <WiFi.h>
+#include <M5GFX.h>
+#include <RTClib.h>
+#include <Wire.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+#include <WiFiMulti.h>
+#include "secrets.h"
+
+RTC_DS3231 rtc;
+TwoWire myWire(1);
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "de.pool.ntp.org");
+
+String daysNames[] = {
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"};
+String monthsNames[] = {
+    "-",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"};
+
+char gears[5] = "RNDX";
+
+M5GFX display;
+M5Canvas canvas(&display);
+M5Canvas canvas_power(&display);
+M5Canvas canvas_state(&display);
+
+CAN_device_t CAN_cfg;             // CAN Config
+const int rx_queue_size = 10;     // Receive Queue size
+
+unsigned long previousMillis = 0; // will store last time a CAN Message was send
+const int interval = 1 * 200;   // interval at which send CAN Messages (milliseconds)
+
+unsigned long previousMillis_WifiReconnect = 0; // will store last time a CAN Message was send
+const int interval_WifiReconnect = 30 * 1000;   // interval at which send CAN Messages (milliseconds)
+
+uint8_t count = 0;
+
+unsigned long previousMillis_RTCsync = 0;
+const int interval_RTCsync = 60 * 1000; // interval at which send CAN Messages (milliseconds)
+bool was_NTPoffline = true;
+
+typedef struct
+{
+    struct
+    {
+        char gear;
+        uint8_t range;
+        bool hand_brake_active;
+        bool ready;
+    } display;
+    struct
+    {
+        float current;
+        float voltage;
+        float power;
+        float power_max;
+        float power_min;
+        uint8_t soc;
+        float voltage12;
+    } batt;
+    struct
+    {
+        float odometer;
+        uint8_t speed;
+    } odo;
+    struct
+    {
+        uint16_t remain_time;
+    } charge;
+} car_data_struct;
+
+car_data_struct car_data;
+
+void init_RTC()
+{
+    myWire.begin(GPIO_NUM_17, GPIO_NUM_16, 100000);
+    if (!rtc.begin(&myWire))
+    {
+        Serial.println("Couldn't find RTC");
+        Serial.flush();
+        canvas.println("Couldn't find RTC");
+        canvas.pushSprite(0, 0);
+        while (1)
+            delay(10);
+    }
+}
+
+void init_wifi()
+{
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        WiFi.begin(ssid, password);
+    }
+}
+
+void reconnect_wifi()
+{
+    WiFi.disconnect();
+    WiFi.reconnect();
+}
+
+void sync_rtc()
+{
+    if (was_NTPoffline)
+    {
+        timeClient.begin(); // Start NTP client
+        was_NTPoffline = false;
+    }
+
+    timeClient.update();                                  // Retrieve current epoch time from NTP server
+    unsigned long unix_epoch = timeClient.getEpochTime(); // Get epoch time
+    rtc.adjust(DateTime(unix_epoch));
+}
+
+
+void decode_display(uint8_t data[8])
+{
+    car_data.display.gear = gears[(uint8_t)((data[0] >> 7) | ((data[1] & 1) << 1))];
+
+    car_data.display.range = (uint8_t)(data[0] & 127);
+
+    car_data.display.ready = (bool)(data[2] & 4);
+
+    car_data.display.hand_brake_active = (bool)(data[3] & 32);
+}
+
+void decode_batt48_state(uint8_t data[8])
+{
+    car_data.batt.current = ((int16_t)(data[1] << 8 | data[0])) / 10.0;
+
+    car_data.batt.voltage = ((int16_t)(data[3] << 8 | data[2])) / 100.0;
+
+    car_data.batt.power =  car_data.batt.voltage * car_data.batt.current;
+
+    if (car_data.batt.power > car_data.batt.power_max )
+    {
+        car_data.batt.power_max = car_data.batt.power;
+    }
+
+    if (car_data.batt.power < car_data.batt.power_min )
+    {
+        car_data.batt.power_min = car_data.batt.power;
+    }
+
+    car_data.batt.soc = (uint8_t)data[5];
+}
+
+void decode_odo(uint8_t data[8])
+{
+    car_data.odo.odometer = ((data[6] << 8 | data[5]) << 8 | data[4]) / 10.0;
+
+    car_data.odo.speed = (uint8_t)data[7];
+}
+
+void decode_charge(uint8_t data[8])
+{
+    car_data.charge.remain_time = (uint16_t)(data[1] << 8 | data[0]);
+}
+
+void decode_batt12_state(uint8_t data[8])
+{
+    car_data.batt.voltage12 = (data[1] << 8 | data[0]) / 100.0;
+}
+
+void parse_can(CAN_frame_t *rx_frame)
+{
+    switch (rx_frame->MsgID)
+    {
+    case 0x713:
+        decode_display(rx_frame->data.u8);
+        break;
+    case 0x580:
+        decode_batt48_state(rx_frame->data.u8);
+        break;
+    case 0x581:
+        decode_odo(rx_frame->data.u8);
+        break;
+    case 0x582:
+        decode_charge(rx_frame->data.u8);
+        break;
+    case 0x593:
+        decode_batt12_state(rx_frame->data.u8);
+        break;
+    default:
+        break;
+    }
+}
+
+void plot_state()
+{
+    int color;
+    String text;
+    canvas_state.clear();
+    if (car_data.display.ready)
+    {
+        color = TFT_GREEN;
+        text = "Ready";
+    }
+    else {
+        color = TFT_RED;
+        text = "Waiting";
+    }
+    canvas_state.fillRoundRect(0, 0, 100, 40, 5, color);
+    canvas_state.setTextColor(TFT_BLACK);
+    canvas_state.setCursor(5, 10);
+    canvas_state.setTextSize(1);
+    canvas_state.print(text);
+
+    if (car_data.display.hand_brake_active)
+    {
+        color = TFT_RED;
+        text = "Break!";
+    }
+    else {
+        color = TFT_GREEN;
+        text = "OK";
+    }
+    canvas_state.fillRoundRect(110, 0, 100, 40, 5, color);
+    canvas_state.setTextColor(TFT_BLACK);
+    canvas_state.setCursor(135, 10);
+    canvas_state.setTextSize(1);
+    canvas_state.print(text);
+    if ((car_data.display.gear == 'N') || (car_data.display.gear == 'D'))
+    {
+        color = TFT_GREEN;
+    }
+    else {
+        color = TFT_RED;
+    }
+    canvas_state.fillRoundRect(220, 0, 100, 40, 5, color);
+    canvas_state.setTextColor(TFT_BLACK);
+    canvas_state.setCursor(255, 5);
+    canvas_state.setTextSize(2);
+    canvas_state.print(car_data.display.gear);
+
+}
+
+void plot_power()
+{
+    const float max_power = 3000;
+    const float min_power = 10000;
+    int angle;
+    canvas_power.clear();
+    canvas_power.drawArc(75, 75, 75, 74, 180, 360, TFT_RED);
+    canvas_power.drawArc(75, 75, 75, 74, 0, 180, TFT_GREEN);
+
+    angle = (int)(180 + abs(car_data.batt.power_min)/min_power*180.0);
+    angle = angle > 360? 360 : angle;
+    canvas_power.drawArc(75, 75, 75, 60, 180, angle, TFT_RED);
+
+    angle = (int)(180 - car_data.batt.power_max/max_power*180.0);
+    angle = angle < 0? 0 : angle;
+    canvas_power.drawArc(75, 75, 75, 60, angle, 180, TFT_GREEN);
+
+    if (car_data.batt.power < 0){
+        angle = (int)(180 + abs(car_data.batt.power)/min_power*180.0);
+        angle = angle > 360? 360 : angle;
+        canvas_power.fillArc(75, 75, 75, 60, 180, angle, TFT_RED);
+        canvas_power.setTextColor(TFT_RED);
+    }
+    else
+    {
+        angle = (int)(180 - car_data.batt.power/max_power*180.0);
+        angle = angle < 0? 0 : angle;
+        canvas_power.fillArc(75, 75, 75, 60, angle, 180, TFT_GREEN);
+        canvas_power.setTextColor(TFT_GREEN);
+    }  
+
+    canvas_power.setCursor(30, 60);
+    canvas_power.setTextSize(1.0);
+    
+    canvas_power.printf("%2.1f", abs(car_data.batt.power)/1000.0);
+    int x = canvas_power.getCursorX();
+    int y = canvas_power.getCursorY();
+    canvas_power.setCursor(x+6, y+6);
+    canvas_power.setTextSize(0.6);
+    canvas_power.print("kW");
+
+    canvas_power.setTextColor(TFT_RED);
+    canvas_power.setCursor(120, 0);
+    canvas_power.setTextSize(0.5);
+    canvas_power.printf("%+2.1f", car_data.batt.power_min/1000.0);
+
+    canvas_power.setTextColor(TFT_GREEN);
+    canvas_power.setCursor(120, 140);
+    canvas_power.setTextSize(0.5);
+    canvas_power.printf("%+2.1f", car_data.batt.power_max/1000.0);
+
+
+}
+
+void setup()
+{
+    M5.begin();
+    Serial.begin(115200); // This high baud rate is recommended if you want to
+    // debug CAN data. These are coming at quite high rates.
+
+    Serial.println("Basic Demo - AMI-Display");
+
+    display.begin();
+
+    canvas.setColorDepth(1); // mono color
+    canvas.createSprite(150, 16);
+    // canvas.setFont(&fonts::Font0);
+    canvas.setFont(&fonts::FreeMono12pt7b);
+    canvas.setTextSize(1.0);
+    canvas.setPaletteColor(1, GREEN);
+    // canvas.setTextScroll(true);
+
+    canvas_power.createSprite(160, 151);
+    canvas_power.setFont(&fonts::FreeMonoBold18pt7b);
+    canvas_power.setTextScroll(false);
+
+    canvas_state.createSprite(320, 50);
+    canvas_state.setFont(&fonts::FreeMonoBold9pt7b);
+    canvas_state.setTextScroll(false);
+
+    init_wifi();
+
+    CAN_cfg.speed = CAN_SPEED_500KBPS;
+    CAN_cfg.tx_pin_id = GPIO_NUM_26;
+    CAN_cfg.rx_pin_id = GPIO_NUM_36;
+    CAN_cfg.rx_queue = xQueueCreate(rx_queue_size, sizeof(CAN_frame_t));
+    // Init CAN Module
+    canvas.println("Init CAN Module.....");
+    canvas.pushSprite(0, 0);
+    ESP32Can.CANInit();
+
+    init_RTC();
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        /* Pushing that date/time to the RTC */
+        sync_rtc();
+    }
+
+
+}
+
+
+void loop()
+{
+    CAN_frame_t rx_frame;
+
+    unsigned long currentMillis = millis();
+
+    // if (WiFi.status() != WL_CONNECTED) {
+    //     if (currentMillis - previousMillis_WifiReconnect >= interval_WifiReconnect
+    //         || currentMillis < previousMillis_WifiReconnect) {
+    //             reconnect_wifi();
+    //     }
+    // }
+
+    // Receive next CAN frame from queue
+    if (xQueueReceive(CAN_cfg.rx_queue, &rx_frame, 1 * portTICK_PERIOD_MS) == pdTRUE)
+    {
+        if (rx_frame.FIR.B.RTR != CAN_RTR)
+        {
+            parse_can(&rx_frame);
+        }
+    }
+
+    // // Display CAN Data
+    if (currentMillis - previousMillis >= interval)
+    {
+        previousMillis = currentMillis;
+        DateTime dt = rtc.now();
+
+        /* printing that data to the Serial port in a meaningful format */
+        // Serial.println("************");
+        // Serial.print(daysNames[dt.dayOfTheWeek()]);
+        // Serial.print(" ");
+        // Serial.print(monthsNames[dt.month()]);
+        // Serial.print(" ");
+        // Serial.print(dt.day());
+        // Serial.print(", ");
+        // Serial.println(dt.year());
+
+        // Serial.print(dt.hour());
+        // Serial.print(":");
+        // Serial.print(dt.minute());
+        // Serial.print(":");
+        // Serial.println(dt.second());
+
+        canvas.clear();
+        canvas.setCursor(0,0);
+        canvas.printf("%02i", dt.hour());
+        canvas.print(":");
+        canvas.printf("%02i", dt.minute());
+        canvas.print(":");
+        canvas.printf("%02i", dt.second());
+        canvas.println("");
+        canvas.pushSprite(0, 0);
+
+        plot_power();
+        canvas_power.pushSprite(75, 40);
+
+        plot_state();
+        canvas_state.pushSprite(0, 200);
+    }
+    // delay(1);
+
+    // if (currentMillis - previousMillis_RTCsync >= interval_RTCsync || currentMillis < previousMillis_RTCsync) {
+    //     previousMillis_RTCsync = currentMillis;
+    //     if (WiFi.status() == WL_CONNECTED) {
+    //         sync_rtc();
+    //         canvas.println("Synced RTC");
+    //         canvas.pushSprite(0, 0);
+    //     }
+    //     else
+    //     {
+    //         canvas.println("Was offline");
+    //         canvas.pushSprite(0, 0);
+    //         was_NTPoffline = true;
+    //     }
+    // }
+}
+
+void managing_loop()
+{
+}
